f8328b3cae085b7122bd3f1f43bfb0714e50e2aa
fengjianzhi <fengjianzhi@outlook.com>
2017-03-24 21:44:19 +0800

    implement groups#new

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324122217) do
+ActiveRecord::Schema.define(version: 20170324133606) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
-    t.text     "descrition"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
